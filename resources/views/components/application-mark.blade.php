07b89f0a603292f079f2c7bbc55d8b7d9a150208
ys-grouse <malsawma7777@gmail.com>
2024-01-20 12:30:43 +0530

    feat[update]: logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,65 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 100 100"
+    xml:space="preserve">
+    <defs>
+    </defs>
+    <rect x="0" y="0" width="100%" height="100%" fill="rgba(255,255,255,0)" />
+    <g transform="matrix(1.0078 0 0 1.0078 50.2485 49.9999)" id="668736">
+        <g style="">
+            <g transform="matrix(0.1435 0 0 0.1435 -7.9297 -17.6287)" id="123899">
+                <radialGradient id="SVGID_20" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 0 0)"
+                    cx="470.5434" cy="333.0651" r="267.8458" fx="470.5434" fy="333.0651">
+                    <stop offset="0%" style="stop-color:rgb(91,203,245);stop-opacity: 1" />
+                    <stop offset="52.42%" style="stop-color:rgb(41,148,195);stop-opacity: 1" />
+                    <stop offset="100%" style="stop-color:rgb(0,102,153);stop-opacity: 1" />
+                </radialGradient>
+                <path
+                    style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_20); fill-rule: nonzero; opacity: 1;"
+                    transform=" translate(-599.4375, -370.5631)"
+                    d="M 361 518.373 c 145 -107.507 306.061 -191.637 476.875 -248.529 c -1.003 -0.314 -2.086 -0.587 -3.132 -0.807 l -217.444 -45.59 c -4.41 -0.925 -8.979 -0.925 -13.39 0 l -217.213 45.59 C 371.605 272.202 361 285.51 361 300.929 v 166.583 v -0.217 V 518.373 z"
+                    stroke-linecap="round" />
+            </g>
+            <g transform="matrix(0.1435 0 0 0.1435 0 0.109)" id="90519">
+                <radialGradient id="SVGID_21" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 0 0)"
+                    cx="546.6729" cy="431.1689" r="329.446" fx="546.6729" fy="431.1689">
+                    <stop offset="0%" style="stop-color:rgb(0,189,242);stop-opacity: 1" />
+                    <stop offset="59.18%" style="stop-color:rgb(0,102,153);stop-opacity: 1" />
+                    <stop offset="100%" style="stop-color:rgb(0,43,100);stop-opacity: 1" />
+                </radialGradient>
+                <path
+                    style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_21); fill-rule: nonzero; opacity: 1;"
+                    transform=" translate(-654.7245, -494.1695)"
+                    d="M 1002.079 224.271 c -48.072 21.927 -95.497 45.675 -141.78 71.165 c -0.005 -0.031 -0.106 -0.062 -0.112 -0.093 c -0.084 0.056 -0.187 0.093 -0.187 0.093 c -158.68 87.39 -305.984 195.373 -437.223 320.235 l 0.189 0.329 h -0.212 c 0 0 -53.887 52.614 -115.384 130.61 c 0 0 79.842 -80.837 132.121 -116.884 L 589.56 756.39 c 12.14 10.25 29.903 10.231 42.043 -0.019 L 849.227 572.8 C 856.56 566.609 861 557.49 861 547.893 V 324.892 C 919 269.705 1002.079 224.271 1002.079 224.271 z"
+                    stroke-linecap="round" />
+            </g>
+            <g transform="matrix(0.1435 0 0 0.1435 -15.23 -20.2899)" id="222632">
+                <linearGradient id="SVGID_22" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 0 0)"
+                    x1="550.7165" y1="364.6754" x2="488.3051" y2="256.5756">
+                    <stop offset="0%" style="stop-color:rgb(0,189,242);stop-opacity: 0" />
+                    <stop offset="11.51%" style="stop-color:rgb(10,191,243);stop-opacity: 0.1151" />
+                    <stop offset="30.95%" style="stop-color:rgb(36,196,244);stop-opacity: 0.3095" />
+                    <stop offset="55.94%" style="stop-color:rgb(79,205,246);stop-opacity: 0.5594" />
+                    <stop offset="85.22%" style="stop-color:rgb(138,218,248);stop-opacity: 0.8522" />
+                    <stop offset="100%" style="stop-color:rgb(171,225,250);stop-opacity: 1" />
+                </linearGradient>
+                <path
+                    style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_22); fill-rule: nonzero; opacity: 1;"
+                    transform=" translate(-548.543, -351.9965)"
+                    d="M 401 306.981 l 209.903 -44.063 l 85.183 17.829 C 592.88 324.485 494 378.104 401 441.075 V 306.981 z"
+                    stroke-linecap="round" />
+            </g>
+            <g transform="matrix(0.1435 0 0 0.1435 -0.3495 7.1254)" id="189011">
+                <radialGradient id="SVGID_23" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 0 0)"
+                    cx="575.662" cy="492.2661" r="230.743" fx="575.662" fy="492.2661">
+                    <stop offset="0%" style="stop-color:rgb(0,189,242);stop-opacity: 1" />
+                    <stop offset="59.18%" style="stop-color:rgb(0,102,153);stop-opacity: 1" />
+                    <stop offset="100%" style="stop-color:rgb(0,43,100);stop-opacity: 1" />
+                </radialGradient>
+                <path
+                    style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_23); fill-rule: nonzero; opacity: 1;"
+                    transform=" translate(-652.197, -542.991)"
+                    d="M 483.394 614.055 C 587.052 519.61 700 435.86 821 364.234 v 180.352 L 611.133 721.748 L 483.394 614.055 z"
+                    stroke-linecap="round" />
+            </g>
+        </g>
+    </g>
 </svg>
